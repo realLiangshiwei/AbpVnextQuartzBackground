7ca1ef3f3ac576c0898b38628f83e18e60c81ec5
liangshiwei <liangshiw@outlook.com>
2020-04-27 16:34:28 +0800

    Create QuartzBackgroundWorkerManager.cs

---
+++ QuartzBackgroundWorkerManager.cs
@@ -0,0 +1,37 @@
+[Dependency(ReplaceServices = true)]
+public class QuartzBackgroundWorkerManager : IBackgroundWorkerManager, ISingletonDependency
+{
+    private readonly IScheduler _scheduler;
+
+    public QuartzBackgroundWorkerManager(IScheduler scheduler)
+    {
+        _scheduler = scheduler;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken = default)
+    {
+        await _scheduler.ResumeAll(cancellationToken);
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken = default)
+    {
+        if (!_scheduler.IsShutdown)
+        {
+            await _scheduler.PauseAll(cancellationToken);
+        }
+    }
+
+    public void Add(IBackgroundWorker worker)
+    {
+        if (await _scheduler.CheckExists(quartzWork.JobDetail.Key))
+        {
+            await _scheduler.AddJob(quartzWork.JobDetail, true, true);
+            await _scheduler.ResumeJob(quartzWork.JobDetail.Key);
+            await _scheduler.RescheduleJob(quartzWork.Trigger.Key, quartzWork.Trigger);
+        }
+        else
+        {
+            await _scheduler.ScheduleJob(quartzWork.JobDetail, quartzWork.Trigger);
+        }
+    }
+}
